d8f2a2440a45b426c96dd64848452a26d2a11464
Platon61633 <peredreevplaton@gmail.com>
2023-12-27 21:38:31 +0300

    c

--- api/index.php
+++ api/index.php
@@ -16,6 +16,7 @@ switch ($_GET['need']) {
     case 'ns':
         $ns = mysqli_fetch_all(mysqli_query($connect, 'SELECT * FROM `NS`'));
         $trainsOnFirstWay = explode(' ',$ns[0][1]);
+        print_r($trainsOnFirstWay)
         for ($i=0; $i < count($trainsOnFirstWay); $i++) { 
             echo $trainsOnFirstWay[$i];
         }
