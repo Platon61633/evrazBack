9ea3879012da61e72f66c278415f4c953f3c95fa
Platon61633 <peredreevplaton@gmail.com>
2023-12-27 22:30:37 +0300

    c

--- api/index.php
+++ api/index.php
@@ -19,10 +19,11 @@ switch ($_GET['need']) {
         for ($i=0; $i < count($ns); $i++) { 
             for ($j=0; $j < count(explode(' ',$ns[$i][1])); $j++) { 
                 // echo explode(' ',$ns[$i][1])[$j].', ';
-                $trains[$i][$j] = mysqli_fetch_all(mysqli_query($connect, "SELECT * FROM `trains` WHERE `number` = ". explode(' ',$ns[$i][1])[$j]));
+                $trains[$i][$j] = mysql_fetch_array(mysqli_query($connect, "SELECT * FROM `trains` WHERE `number` = ". explode(' ',$ns[$i][1])[$j]));
             }
             // echo '-----------------';
         }
+        
         print_r($trains);
         
         // explode(' ',$ns[0][1])
