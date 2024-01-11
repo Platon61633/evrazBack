e893b8688b0cff11aa29352364d7649dbb2529a9
Platon61633 <peredreevplaton@gmail.com>
2024-01-11 18:47:37 +0300

    ggggggggggg

--- api/index.php
+++ api/index.php
@@ -19,12 +19,12 @@ switch ($_GET['need']) {
                             $trains[$i] = 0;
                         }else {
                             for ($j=0; $j < count($trainsOnWay); $j++) { 
-                                echo "SELECT * FROM `trains` WHERE `number` = ". explode(' ',$ns[$i][1])[$j].'<br/>';
-                                if ($ns[$i][1]) {
-                                    echo $ns[$i][1].'<br/>';
-                                }else {
-                                    echo 'nooooo';
-                                }
+                                // echo "SELECT * FROM `trains` WHERE `number` = ". explode(' ',$ns[$i][1])[$j].'<br/>';
+                                // if ($ns[$i][1]) {
+                                //     echo $ns[$i][1].'<br/>';
+                                // }else {
+                                //     echo 'nooooo';
+                                // }
                                 
                                 $trains[$i][$j] = mysqli_fetch_all(mysqli_query($connect, "SELECT * FROM `trains` WHERE `number` = ". explode(' ',$ns[$i][1])[$j]))[0];
                             }
