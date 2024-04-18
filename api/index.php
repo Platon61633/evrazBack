c043ab99a15663fb23b201639942b3c27bfa3cb3
Platon61633 <peredreevplaton@gmail.com>
2024-04-18 13:58:27 +0300

    c

--- api/index.php
+++ api/index.php
@@ -12,9 +12,10 @@ switch ($_GET['need']) {
         switch ($method) {
             case 'GET':
                 $id = $_GET['id'];
+                $station = $_GET['station']
                 $password = mysqli_fetch_all(mysqli_query($connect, "SELECT `password` FROM `amdins` WHERE `id`='$id';"))[0][0];
                 if ($password==$_GET['password']) {
-                    $ns = mysqli_fetch_all(mysqli_query($connect, 'SELECT * FROM `ns`'));
+                    $ns = mysqli_fetch_all(mysqli_query($connect, 'SELECT * FROM `'.$station.'`'));
                 for ($i=0; $i < count($ns); $i++) {
                     $trainsOnWay = explode(' ',$ns[$i][1]);
                     $CHlocoOnWay = explode(' ',$ns[$i][2]);
