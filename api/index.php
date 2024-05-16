57a9d6dea6c6c30cbfce78856e9a98eca5ea812b
Platon61633 <peredreevplaton@gmail.com>
2024-05-16 23:09:34 +0300

    f

--- api/index.php
+++ api/index.php
@@ -15,7 +15,7 @@ switch ($_GET['need']) {
                 $id = $_GET['id'];
                 
                 $password = mysqli_fetch_all(mysqli_query($connect, "SELECT `password` FROM `amdins` WHERE `id`='$id';"))[0][0];
-                echo mysqli_fetch_all(mysqli_query($connect, "SELECT `password` FROM `amdins` WHERE `id`='$id';"));
+                print_r(mysqli_fetch_all(mysqli_query($connect, "SELECT `password` FROM `amdins` WHERE `id`='$id';")));
                 if ($password==$_GET['password']) {
                     $ns = mysqli_fetch_all(mysqli_query($connect, "SELECT * FROM `".$station."`"));
                 for ($i=0; $i < count($ns); $i++) {
