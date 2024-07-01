fb307e14ec1a85cea3a62e1284338e26c7ffe38e
Platon61633 <peredreevplaton@gmail.com>
2024-07-01 15:06:11 +0300

    fcc

--- api/index.php
+++ api/index.php
@@ -17,7 +17,7 @@ switch ($_GET['need']) {
         switch ($method) {
             case 'GET':
                 $id = $_GET['id'];
-                
+                echo $password;
                 $password = mysqli_fetch_all(mysqli_query($connect, "SELECT `password` FROM `amdins` WHERE `id`='$id';"))[0][0];
                 if ($password==$_GET['password']) {
                     $ns = mysqli_fetch_all(mysqli_query($connect, "SELECT * FROM `".$station."`"));
