39fd9147738fce0194006c6e39a7b1df86a8d219
Platon61633 <peredreevplaton@gmail.com>
2024-07-11 01:53:18 +0300

    gg

--- api/index.php
+++ api/index.php
@@ -15,7 +15,7 @@ switch ($_GET['need']) {
     case 'signin':
         switch ($method) {
             case 'POST':
-                $data = json_decode(file_get_contents('php://input'), true);
+                $data = file_get_contents('php://input');
 
                 // echo $data['name'];
 
