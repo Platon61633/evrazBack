c288d9d6e4243c2adc9e4d9cdafaf18f495ca28f
Platon61633 <peredreevplaton@gmail.com>
2024-07-11 01:27:49 +0300

    true

--- api/index.php
+++ api/index.php
@@ -15,7 +15,7 @@ switch ($_GET['need']) {
     case 'register':
         switch ($method) {
             case 'POST':
-                $data = json_decode(file_get_contents('php://input'));
+                $data = json_decode(file_get_contents('php://input'), true);
                 echo $data['name'];
                 print_r($data);
                 break;
