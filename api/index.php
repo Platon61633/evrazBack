cebf6b535b2c70ab6cfb3d63c68f64bc7c182f0b
Platon61633 <peredreevplaton@gmail.com>
2024-07-11 15:05:06 +0300

    encode

--- api/index.php
+++ api/index.php
@@ -59,7 +59,7 @@ switch ($_GET['need']) {
             case 'GET':
                 $data = mysqli_fetch_all(mysqli_query($connect1, "SELECT * FROM `posts`"));
                 print_r($data);
-                echo $data;
+                echo json_encode($data);
                 break;
             
             default:
