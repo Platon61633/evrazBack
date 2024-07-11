4725b7c34d680d8ed3799e86e89282d3e5b2d0d9
Platon61633 <peredreevplaton@gmail.com>
2024-07-12 00:08:25 +0300

    gg

--- api/index.php
+++ api/index.php
@@ -73,22 +73,22 @@ switch ($_GET['need']) {
             case 'POST':
                 $data = json_decode(file_get_contents('php://input'), true);
                 $Liked = '';
-                for ($i=0; $i < $data['Liked']; $i++) { 
-                    $Liked = $Liked.$data['Liked'][$i].',';
-                }
-                mysqli_query($connect1, "UPDATE `users` SET `liked` = '".$Liked."' WHERE `users`.`id` = ".$data['id_user'].";");
-                break;
-            case "GET":
-                echo 'k';
-                $data = ['Liked'=>['1'],
-            'id_user'=>'2'
-            ];
-                $Liked = '';
                 for ($i=0; $i < count($data['Liked']); $i++) { 
                     $Liked = $Liked.$data['Liked'][$i].',';
                 }
                 mysqli_query($connect1, "UPDATE `users` SET `liked` = '".$Liked."' WHERE `users`.`id` = ".$data['id_user'].";");
                 break;
+            // case "GET":
+            //     echo 'k';
+            //     $data = ['Liked'=>['1'],
+            // 'id_user'=>'2'
+            // ];
+            //     $Liked = '';
+            //     for ($i=0; $i < count($data['Liked']); $i++) { 
+            //         $Liked = $Liked.$data['Liked'][$i].',';
+            //     }
+            //     mysqli_query($connect1, "UPDATE `users` SET `liked` = '".$Liked."' WHERE `users`.`id` = ".$data['id_user'].";");
+            //     break;
             
             default:
                 # code...
