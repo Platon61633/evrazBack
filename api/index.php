9e58fe10c3f9af443cee10b97e5abdd82ddca0a3
Platon61633 <peredreevplaton@gmail.com>
2024-07-12 06:29:05 +0300

    gg

--- api/index.php
+++ api/index.php
@@ -133,6 +133,18 @@ switch ($_GET['need']) {
         }
         break;
 
+    case 'create_post':
+        switch ($method) {
+            case 'POST':
+                $data = json_decode(file_get_contents('php://input'), true);
+                print_r($data)
+                break;
+            
+            default:
+                # code...
+                break;
+        }
+
 
 
 
